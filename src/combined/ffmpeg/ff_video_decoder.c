0b54d92332a91c0758f18bb7844233cb9a256b12
root <root@worker.(none)>
2011-09-26 11:42:25 +0200

    fixed broken upstream mpeg12 decoding
    only init the mpeg12 parser, but do set init mode to 0. init mode is set
    in another place where it is correct.

--- src/combined/ffmpeg/ff_video_decoder.c
+++ src/combined/ffmpeg/ff_video_decoder.c
@@ -1065,7 +1065,6 @@ static void ff_init_mpeg12_mode(ff_video_decoder_t *this)
   if ( this->mpeg_parser == NULL ) {
     this->mpeg_parser = calloc(1, sizeof(mpeg_parser_t));
     mpeg_parser_init(this->mpeg_parser);
-    this->decoder_init_mode = 0;
   }
 }
 
